b00c9212e31c4bc4136823be09b11533dd8b7fab
4d4ut0 <adauto_tl@hotmail.comr>
2020-10-24 19:37:22 -0400

    Start project: -login FireBase;

---
+++ app/src/main/java/com/example/themoviedb/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.themoviedb;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Log.d("AUTH", "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    Log.d("AUTH", "onAuthStateChanged:signed_out");
+                }
+
+            }
+        };
+
+        mAuth.signInWithEmailAndPassword("teste@teste.com", "teste123").addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (!task.isSuccessful()) {
+                    Log.w("AUTH", "Falha ao efetuar o Login: ", task.getException());
+                }else{
+                    Log.d("AUTH", "Login Efetuado com sucesso!!!");
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+}
