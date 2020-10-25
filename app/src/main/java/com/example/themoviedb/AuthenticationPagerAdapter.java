9c8f7c4704fe5f074975fefd8ff7b079f07cf49a
4d4ut0 <adauto_tl@hotmail.comr>
2020-10-24 20:19:32 -0400

    Tela de login e registro em progresso: -Fragments criadas; -Color e strings defindos;

--- app/src/main/java/com/example/themoviedb/AuthenticationPagerAdapter.java
+++ app/src/main/java/com/example/themoviedb/AuthenticationPagerAdapter.java
@@ -1,4 +1,31 @@
 package com.example.themoviedb;
 
-public class AuthenticationPagerAdapter {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class AuthenticationPagerAdapter extends FragmentPagerAdapter {
+    private ArrayList<Fragment> fragmentList = new ArrayList<>();
+
+    public AuthenticationPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int i) {
+        return fragmentList.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+    void addFragmet(Fragment fragment) {
+        fragmentList.add(fragment);
+    }
 }
