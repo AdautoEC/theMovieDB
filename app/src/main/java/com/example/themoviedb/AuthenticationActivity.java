7b7e48018a46b1bb0ba0e4757ea051af2cb38dff
4d4ut0 <adauto_tl@hotmail.comr>
2020-10-25 05:09:07 -0400

    .

---
+++ app/src/main/java/com/example/themoviedb/AuthenticationActivity.java
@@ -0,0 +1,23 @@
+package com.example.themoviedb;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = findViewById(R.id.viewPager);
+
+        AuthenticationPagerAdapter pagerAdapter = new AuthenticationPagerAdapter(getSupportFragmentManager());
+        pagerAdapter.addFragmet(new LoginFragment());
+        pagerAdapter.addFragmet(new RegisterFragment());
+        viewPager.setAdapter(pagerAdapter);
+    }
+
+}
